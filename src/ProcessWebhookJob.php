7f68060d7e31e0fad2dd46b92d25668779809d0b
Elan Clarkson <elanclarkson@gmail.com>
2021-06-29 17:53:54 +1200

    Remove SerializesModels from ProcessWebhookJob

--- src/ProcessWebhookJob.php
+++ src/ProcessWebhookJob.php
@@ -6,12 +6,11 @@ use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
-use Illuminate\Queue\SerializesModels;
 use Spatie\WebhookClient\Models\WebhookCall;
 
 abstract class ProcessWebhookJob implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable, InteractsWithQueue, Queueable;
 
     public WebhookCall $webhookCall;
 
